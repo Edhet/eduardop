c1ffedf17816c12c035a2d6f6309b7102dfb0e58
Eduardo P <edtvports@gmail.com>
2023-02-04 17:19:54 -0300

    Project filters

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,38 +1,114 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as data from '../../assets/json/projects.json';
-import {timeSinceInMicros} from "@angular/compiler-cli/src/ngtsc/perf/src/clock";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.css']
 })
-export class ProjectCardComponent {
+export class ProjectCardComponent implements OnInit {
   static _projectsHeader: string = "Projects";
   projectsJSON = (data as any).default;
   projects = new Array<Project>;
+  languages = new Array<Language>;
+  activeFilters = new Array<Language>;
 
-  constructor() {
+  constructor() { }
+
+  ngOnInit() {
+    this.populateProjects();
+  }
+
+  populateProjects() {
+    this.projects = new Array<Project>;
+
     for (const i in this.projectsJSON) {
-      let tempProject: Project = {name: "", image: "", url: "", languages: []};
       let projLanguages = new Array<Language>;
 
       for (const j in this.projectsJSON[i].languages) {
-
-        let tempLang: Language = {name: "", color: "", bgColor: ""};
-        tempLang.name = this.projectsJSON[i].languages[j].langName;
-        tempLang.color = this.projectsJSON[i].languages[j].color;
-        tempLang.bgColor = this.projectsJSON[i].languages[j].bgColor;
+        let tempLang = {
+          name: this.projectsJSON[i].languages[j].langName,
+          color: this.projectsJSON[i].languages[j].color,
+          bgColor:  this.projectsJSON[i].languages[j].bgColor
+        };
         projLanguages.push(tempLang);
+        if (this.langIsUnique(tempLang))
+          this.languages.push(tempLang);
       }
 
-      tempProject.name = this.projectsJSON[i].name;
-      tempProject.image = this.projectsJSON[i].image;
-      tempProject.url = this.projectsJSON[i].url;
-      tempProject.languages = projLanguages;
-
-      this.projects.push(tempProject);
+      this.projects.push({
+        name: this.projectsJSON[i].name,
+        image: this.projectsJSON[i].image,
+        url: this.projectsJSON[i].url,
+        languages: projLanguages
+      });
     }
+  }
+
+  onClick(langName: string) {
+    let notActive = true;
+    this.activeFilters.forEach((filter, index) => {
+      if (langName == filter.name) {
+        notActive = false;
+        this.removeFilter(langName, index);
+      }
+    });
+    if (notActive)
+      this.applyFilter(langName);
+    this.filterProjects();
+  }
+
+  removeFilter(langName: string, index: number) {
+    this.languages.forEach(lang => {
+      if (lang.name == langName) {
+        lang.hasBorder = false;
+        lang.borderColor = undefined;
+      }
+    });
+    this.activeFilters.splice(index, 1);
+  }
+
+  applyFilter(langName: string) {
+    this.languages.forEach(lang => {
+      if (lang.name == langName) {
+        lang.hasBorder = true;
+        lang.borderColor = "outline" + lang.bgColor.slice(2, lang.bgColor.length-3);
+        this.activeFilters.push(lang);
+      }
+    });
+  }
+
+  filterProjects() {
+    this.populateProjects();
+    if (this.activeFilters.length <= 0)
+      return;
+
+    for (let prjIndex = 0; prjIndex < this.projects.length; prjIndex++) {
+      let shouldRemove = false;
+      for (let filterIndex = 0; filterIndex < this.activeFilters.length; filterIndex++) {
+        let notHasFilter = true;
+        for (let langIndex = 0; langIndex < this.projects[prjIndex].languages.length; langIndex++) {
+          if (this.projects[prjIndex].languages[langIndex].name == this.activeFilters[filterIndex].name) {
+            notHasFilter = false;
+            break;
+          }
+        }
+        if (notHasFilter)
+          shouldRemove = true;
+      }
+      if (shouldRemove) {
+        this.projects.splice(prjIndex, 1);
+        prjIndex--;
+      }
+    }
+  }
+
+  langIsUnique(lang: Language): boolean {
+    for (let i in this.languages)
+      if (lang.name == this.languages[i].name)
+        return false;
+    return true;
   }
 
   get projectsHeader() {
@@ -50,5 +126,7 @@ interface Project {
 interface Language {
   name:string,
   color:string,
-  bgColor:string
+  bgColor:string,
+  hasBorder?: boolean,
+  borderColor?:string
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,53 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   safelist: [
-    'text-orange-500',
-    'bg-orange-500/25',
+    'text-orange-600',
+    'bg-orange-600/25',
+    'outline-orange-600',
+
+    'text-orange-400',
+    'bg-orange-400/25',
+    'outline-orange-400',
+
+    'text-rose-600',
+    'bg-rose-600/25',
+    'outline-rose-600',
+
+    'text-sky-600',
+    'bg-sky-600/25',
+    'outline-sky-600',
+
     'text-yellow-400',
     'bg-yellow-400/25',
+    'outline-yellow-400',
+
+    'text-yellow-300',
+    'bg-yellow-300/25',
+    'outline-yellow-300',
+
     'text-cyan-500',
     'bg-cyan-500/25',
+    'outline-cyan-500',
+
     'text-red-500',
     'bg-red-500/25',
+    'outline-red-500',
+
     'text-blue-500',
     'bg-blue-500/25',
+    'outline-blue-500',
+
     'text-blue-400',
     'bg-blue-400/25',
+    'outline-blue-400',
+
     'text-purple-600',
     'bg-purple-600/25',
+    'outline-purple-600',
+
     'text-amber-300',
     'bg-amber-300/25',
+    'outline-amber-300',
   ],
   theme: {
     extend: {},
