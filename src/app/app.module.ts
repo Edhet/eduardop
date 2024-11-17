91749a285c13e8ba7cf46c57834240c158022cbe
Eduardo P. <edtvports@gmail.com>
2024-11-17 09:09:15 -0300

    Renamed hero folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProjectListComponent } from './components/project-list/project-list.component';
-import { HeroComponent } from './components/header/hero.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SkillsAndExperienceComponent } from './components/skills-and-experience/skills-and-experience.component';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
 import { EventTimelineComponent } from './components/event-timeline/event-timeline.component';
 import { TechDisplayComponent } from './components/tech-display/tech-display.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
